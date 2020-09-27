4c1ab5b8ff8767075a1f87b9e73dcc1eea894965
1006722361@qq.com <z9889898>
2020-09-27 15:17:10 +0800

    Initial commit

---
+++ src/test/java/com/yunyun/financemanager/FinanceManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yunyun.financemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
