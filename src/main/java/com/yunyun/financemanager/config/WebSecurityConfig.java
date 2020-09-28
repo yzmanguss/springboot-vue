d4298d2f18906b77997a09d061b08121389546ad
1006722361@qq.com <z9889898>
2020-09-28 13:23:19 +0800

    Security配置

--- src/main/java/com/yunyun/financemanager/config/WebSecurityConfig.java
+++ src/main/java/com/yunyun/financemanager/config/WebSecurityConfig.java
@@ -12,9 +12,12 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -36,7 +39,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf()
+        http
+                .csrf()
                 .disable()
                 .cors()
                 .and()
@@ -48,41 +52,71 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .successHandler(new AuthenticationSuccessHandlerImpl())
                 .failureHandler(new AuthenticationFailureHandlerImpl())
                 .and()
-                .logout()
+                .rememberMe()
                 .and()
-                .rememberMe();
+                .logout()
+                .logoutSuccessHandler(new LogoutSuccessHandlerImpl())
+                .and()
+                .exceptionHandling()
+                .authenticationEntryPoint(new AuthenticationEntryPointImpl());
     }
 
+    /**
+     * 处理登录成功返回
+     */
     public static class AuthenticationSuccessHandlerImpl implements AuthenticationSuccessHandler {
-
         @Override
         public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
                                             Authentication authentication) throws IOException {
-            PrintWriter writer = response.getWriter();
-            response.setStatus(HttpStatus.OK.value());
-            response.setContentType(MediaType.APPLICATION_JSON_VALUE);
-
             Object principal = authentication.getPrincipal();
             String result = OBJECT_MAPPER.writeValueAsString(ApiResponse.ok(principal));
-            writer.write(result);
-            writer.flush();
+            writeStringWithJson(response, result);
         }
-
     }
 
+    /**
+     * 处理登录失败返回
+     */
     public static class AuthenticationFailureHandlerImpl implements AuthenticationFailureHandler {
-
         @Override
         public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
                                             AuthenticationException exception) throws IOException {
-            PrintWriter writer = response.getWriter();
-            response.setStatus(HttpStatus.OK.value());
-            response.setContentType(MediaType.APPLICATION_JSON_VALUE);
-
             String result = OBJECT_MAPPER.writeValueAsString(ApiResponse.failure(ResponseCode.BAD_CREDENTIAL));
-            writer.write(result);
-            writer.flush();
+            writeStringWithJson(response, result);
         }
+    }
+
+    /**
+     * 处理登出成功返回
+     */
+    public static class LogoutSuccessHandlerImpl implements LogoutSuccessHandler {
+        @Override
+        public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response,
+                                    Authentication authentication) throws IOException {
+            String result = OBJECT_MAPPER.writeValueAsString(ApiResponse.ok());
+            writeStringWithJson(response, result);
+        }
+    }
+
+    /**
+     * 处理未登录返回
+     */
+    public static class AuthenticationEntryPointImpl implements AuthenticationEntryPoint {
+        @Override
+        public void commence(HttpServletRequest request, HttpServletResponse response,
+                             AuthenticationException authException) throws IOException {
+            String result = OBJECT_MAPPER.writeValueAsString(ApiResponse.failure(ResponseCode.UNAUTHORIZED));
+            writeStringWithJson(response, result);
+        }
+    }
+
+    @SuppressWarnings("deprecation")
+    private static void writeStringWithJson(HttpServletResponse response, String jsonStr)  throws IOException {
+        response.setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
+        response.setStatus(HttpStatus.OK.value());
+        PrintWriter writer = response.getWriter();
+        writer.write(jsonStr);
+        writer.flush();
     }
 
 }
