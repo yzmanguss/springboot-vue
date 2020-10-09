cadb98456b01fdf993acbc80b8aad5ac5b54a7e6
anguss <1720584431@qq.com>
2020-10-09 15:36:38 +0800

    修改工作类型id  model

--- src/main/java/com/yunyun/financemanager/project/vo/Paticipants.java
+++ src/main/java/com/yunyun/financemanager/project/vo/Paticipants.java
@@ -14,7 +14,7 @@ import java.time.LocalDateTime;
 public class Paticipants {
 
 
-    @ApiModelProperty(value = "工作类型id")
+    @ApiModelProperty(value = "工作类型id 1：需求 2：设计 3：测试 4：开发 5：维护")
     Long typeId;
 
     @ApiModelProperty(value = "成员id")
