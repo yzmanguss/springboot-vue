cd0732af925444d510172624634a0fb8854b9cc8
1006722361@qq.com <z9889898>
2020-10-09 18:31:21 +0800

    文件上传下载

--- src/main/java/com/yunyun/financemanager/file/FileController.java
+++ src/main/java/com/yunyun/financemanager/file/FileController.java
@@ -10,27 +10,23 @@ import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.util.Assert;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.constraints.NotBlank;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.IOException;
 
 /**
  * @author zhaoqin
  */
-@Api("文件上传")
+@Api(tags = "文件上传")
 @Slf4j
 @Validated
-@RestController("/files")
+@RestController
+@RequestMapping("/files")
 public class FileController {
 
     @ApiOperation("上传单个文件")
