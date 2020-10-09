5a92c28cb36b2ab7461446dc68b594cc5293fc03
1006722361@qq.com <z9889898>
2020-10-09 19:55:07 +0800

    修复bug

--- src/main/java/com/yunyun/financemanager/project/mapper/MemberMapper.java
+++ src/main/java/com/yunyun/financemanager/project/mapper/MemberMapper.java
@@ -3,7 +3,6 @@ package com.yunyun.financemanager.project.mapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.yunyun.financemanager.common.entity.Member;
 import org.apache.ibatis.annotations.Mapper;
-import org.springframework.stereotype.Repository;
 
 /**
  * @author yangzhongming
