5042e1030cc4399aed8c559551814c59c142444c
1006722361@qq.com <z9889898>
2020-10-09 19:33:52 +0800

    修复bug

--- src/main/java/com/yunyun/financemanager/common/entity/Reimbursement.java
+++ src/main/java/com/yunyun/financemanager/common/entity/Reimbursement.java
@@ -3,9 +3,17 @@ package com.yunyun.financemanager.common.entity;
 import java.io.Serializable;
 import java.time.LocalDateTime;
 
+import com.baomidou.mybatisplus.annotation.FieldStrategy;
+import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
 
 /**
  * reimbursement
@@ -17,60 +25,71 @@ public class Reimbursement implements Serializable {
     @ApiModelProperty(value = "ID", name = "id")
     private Long id;
 
-    @ApiModelProperty(value = "名字", name = "name")
+    @ApiModelProperty(value = "名字", example = "打车费")
+    @NotBlank
+    @Length(max = 50)
     private String name;
 
-
+    @TableField(insertStrategy = FieldStrategy.NEVER, updateStrategy = FieldStrategy.NEVER)
     private LocalDateTime insertTime;
 
+    @TableField(insertStrategy = FieldStrategy.NEVER, updateStrategy = FieldStrategy.NEVER)
     private LocalDateTime updateTime;
 
     /**
      * 实际金额
      */
-    @ApiModelProperty(value = "实际金额", name = "realAmount")
+    @ApiModelProperty(value = "实际金额")
     private Long realAmount;
 
     /**
      * 报销金额
      */
-    @ApiModelProperty(value = "报销金额", name = "reimburseAmount")
+    @ApiModelProperty(value = "报销金额", example = "2000")
+    @NotNull
+    @Min(1L)
     private Long reimburseAmount;
 
     /**
      * 凭据照片
      */
-    @ApiModelProperty(value = "凭据照片", name = "pics;")
+    @ApiModelProperty(value = "凭据照片，图片路径")
+    @NotBlank
     private String pics;
 
     /**
      * 发生时间
      */
-    @ApiModelProperty(value = "发生时间", name = "occurTime")
+    @ApiModelProperty(value = "发生时间", name = "occurTime", example = "2156432445646")
+    @NotNull
+    @Past
     private LocalDateTime occurTime;
 
     /**
      * 备注
      */
-    @ApiModelProperty(value = "备注", name = "note")
+    @ApiModelProperty(value = "备注")
     private String note;
 
     /**
      * 相关人员
      */
-    @ApiModelProperty(value = "相关人员", name = "members")
-    private Long members;
+    @ApiModelProperty(value = "相关人员")
+    @NotNull
+    private Long member;
 
     /**
      * 项目ID
      */
-    @ApiModelProperty(value = "项目ID", name = "projectId")
+    @ApiModelProperty(value = "项目ID")
+    @NotNull
     private Long projectId;
 
     /**
-     * 类型ID
+     * 报销类型ID
      */
-    @ApiModelProperty(value = "类型ID", name = "reimbursementTypeId")
+    @ApiModelProperty(value = "报销类型ID", example = "1")
+    @NotNull
     private Long reimbursementTypeId;
 
     private static final long serialVersionUID = 1L;
--- src/main/java/com/yunyun/financemanager/system/controller/ReimbursementController.java
+++ src/main/java/com/yunyun/financemanager/system/controller/ReimbursementController.java
@@ -1,15 +1,12 @@
 package com.yunyun.financemanager.system.controller;
 
-import com.yunyun.financemanager.common.dto.ReimbursementDTO;
 import com.yunyun.financemanager.common.entity.Reimbursement;
 import com.yunyun.financemanager.common.response.ApiResponse;
 import com.yunyun.financemanager.system.service.impl.ReimbursementServiceImpl;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -26,13 +23,9 @@ public class ReimbursementController {
     private final ReimbursementServiceImpl reimbursementService;
 
     @ApiOperation("插入报销")
-    @PostMapping(value = "/Reimbursement", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public ApiResponse<Void> insertReimbursement(@RequestBody @Validated ReimbursementDTO reimbursementDTO) {
-
-        Reimbursement reimbursement = new Reimbursement();
-        BeanUtils.copyProperties(reimbursementDTO, reimbursement);
-        reimbursementService.insertReimbursement(reimbursement, reimbursementDTO.getPhoto());
-
+    @PostMapping(value = "/Reimbursement")
+    public ApiResponse<Void> insertReimbursement(@RequestBody @Validated Reimbursement reimbursement) {
+        reimbursementService.insertReimbursement(reimbursement);
         return ApiResponse.ok();
     }
 }
