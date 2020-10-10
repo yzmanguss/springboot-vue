864dd7c9c8aae39d0d62a34935b500df0f79ce79
1018654650@qq.com <285831598@qq.com>
2020-10-10 15:55:55 +0800

    2020年10月10日 修复姓名

--- src/main/java/com/yunyun/financemanager/project/service/impl/ProjectFinanceServiceImpl.java
+++ src/main/java/com/yunyun/financemanager/project/service/impl/ProjectFinanceServiceImpl.java
@@ -41,7 +41,7 @@ public class ProjectFinanceServiceImpl implements ProjectFinanceService {
     public ProjectFinance selectProjectName(int id) {
 
         Project project = projectFinanceMapper.selectProjectName(id);
-
+//        得到工作量
         List<WorkLoad> workLoads = projectFinanceMapper.selectWorkLoadByProjectId(id);
         long longKF = 0L;
         long longCS = 0L;
@@ -60,11 +60,10 @@ public class ProjectFinanceServiceImpl implements ProjectFinanceService {
                     long doc = w.getDailyOfficeCost();
                     longCS += +(dw + doc) * wl;
 
-                } else if (w.getMemberId().equals(project.getLeaderId())) {
-
+                }
+                    //查询员工名字
                     mName = memberMapper.selectMemberNameById(project.getLeaderId());
 
-                }
             }
         }
 
@@ -132,6 +131,7 @@ public class ProjectFinanceServiceImpl implements ProjectFinanceService {
         ProjectFinanceDTO projectFinanceDTO = new ProjectFinanceDTO();
         projectFinanceDTO.setTotal(count);
         projectFinanceDTO.setProjectFinances(pf);
+
 
         return projectFinanceDTO;
     }
