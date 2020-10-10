3b4edabca1b7e32c8b23feca355f335e38659edf
1018654650@qq.com <285831598@qq.com>
2020-10-10 14:02:09 +0800

    2020年10月9日 14:35:16 下午 修复财务

--- src/main/java/com/yunyun/financemanager/project/service/EarlyWarningService.java
+++ src/main/java/com/yunyun/financemanager/project/service/EarlyWarningService.java
@@ -4,7 +4,14 @@ import com.yunyun.financemanager.project.qo.EarlyWarning;
 
 
 
+/**
+ * @author hhr
+ */
 public interface EarlyWarningService {
 
+     /**
+      * 查询预警
+      * @return 预警查询对象
+      */
      EarlyWarning selectEarlyWarning(Long id,long contractId);
 }
